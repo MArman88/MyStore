ed3d11dc5d8524c4438814c901e688bd7acdc20b
A. N. M. Mehbube Arman <mehbubearman@gmail.com>
2023-05-28 21:37:52 +0600

    validation added in the login page.

--- js/login.js
+++ js/login.js
@@ -6,11 +6,44 @@ const btnLogin = document.getElementById('btnLogin');
 const toastBody = document.getElementById('toastBody');
 const toastDiv = document.getElementById('toastDiv');
 
+function isPasswordMatch() {
+    let isPasswordGiven = (passwordInput.value || '').length > 0
+    let passwordMatch = passwordInput.value == passwordConfirmInput.value;
+    passwordInput.classList.remove('is-invalid')
+    if (!isPasswordGiven) {
+        passwordInput.classList.add('is-invalid')
+    }
+    passwordConfirmInput.classList.remove('is-invalid');
+    if (!passwordMatch) {
+        passwordConfirmInput.classList.add('is-invalid');
+    }
+}
+
+function isEmailValid() {
+    let email = emailInput.value || '';
+    emailInput.classList.remove('is-invalid');
+    const dotPosition = email.indexOf('.');
+    const atPosition = email.indexOf('@');
+    let isValidEmail = email.length > 0 && atPosition != -1 && dotPosition != -1 && atPosition < dotPosition && (dotPosition + 1) < email.length;
+    if (!isValidEmail) {
+        emailInput.classList.add('is-invalid');
+    }
+}
+
+passwordConfirmInput.addEventListener('focus', isPasswordMatch);
+passwordInput.addEventListener('focus', isPasswordMatch);
+
+passwordInput.addEventListener('keyup', isPasswordMatch);
+passwordConfirmInput.addEventListener('keyup', isPasswordMatch);
+
+emailInput.addEventListener('focus', isEmailValid);
+emailInput.addEventListener('keyup', isEmailValid);
+
+
 btnLogin.addEventListener('click', () => {
     let email = emailInput.value;
     let password = passwordInput.value;
     let passwordConfirm = passwordConfirmInput.value;
-
 
     if (password == passwordConfirm) {
         if (isReadyForLogin(email, password)) {
